61f8401ad5cfecf7f98e91851ab95c0c890df4ea
julian gaspar <juliangaspar15@gmail.com>
2022-11-14 11:46:41 +0100

    Stupid Coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    @question = params[:question]
+    @answer = coach_answer(@question)
+  end
+
+  def coach_answer(question)
+    if question.downcase == 'I am going to work right now!'
+      'You are great!'
+    elsif question.end_with?('?')
+      'Silly question, get dressed and go to work!'
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+end
